c471f6e5a0652d29887226777d71b26417d6f5b4
stan1812 <2314980309@qq.com>
2017-10-18 17:57:43 +0800

    add basic router and basci page

--- node-demo/routes/index.js
+++ node-demo/routes/index.js
@@ -3,9 +3,36 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'hello,Node' });
 });
+router.get('/list',function(req,res){
+  res.render('list',{
+    title:'list',
+    items:[2017,'libiao','express','nodejs']
+  })
+})
 router.get('/hello',function(req,res){
   res.send('the time is '+ new Date().toString())
+})
+router.get('/user/:username',function(req,res){
+  res.send('user:'+req.params.username);
+})
+router.post('/post',function(req,res){
+  res.send('post success')
+})
+router.get('reg',function(req,res){
+  res.send('get reg suc')
+})
+router.post('reg',function(req,reg){
+  res.send('doreg success')
+})
+router.get('login',function(req,res){
+  res.send('get log succ')
+})
+router.post('login',function(req,res){
+  res.send('done login')
+})
+router.get('logout',function(req,res){
+  res.send('logout')
 })
 module.exports = router;
