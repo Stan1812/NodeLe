9ae1a01d43c558ce912530eb0ba97b19af5e2b57
stan1812 <2314980309@qq.com>
2017-10-23 23:06:29 +0800

    delete password

--- nodemysql/test.js
+++ nodemysql/test.js
@@ -2,7 +2,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'KG1415926535',
+  password : '',
   database : 'nodedb'
 });
  
