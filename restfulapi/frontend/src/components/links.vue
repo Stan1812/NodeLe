571df6eaf08a95271eb41db01f7796133b681e53
stan1812 <2314980309@qq.com>
2017-11-27 18:17:15 +0800

    add link page

--- restfulapi/frontend/src/components/links.vue
+++ restfulapi/frontend/src/components/links.vue
@@ -1,18 +1,19 @@
 <template>
 <div>
   <div id="header">
-        <mu-paper>
-    <mu-appbar title="友情链接">
-        <mu-icon-button icon="close" @click.native="goback" slot="left" />
-    </mu-appbar>
+    <mu-paper>
+      <mu-appbar title="友情链接">
+          <mu-icon-button icon="close" @click.native="goback" slot="left" />
+      </mu-appbar>
     </mu-paper>
   </div>
   <div id="linkList">
-    <mu-list>
-        <mu-sub-header>友情链接</mu-sub-header>
+    <mu-list v-for="(link ,index) in links" :key="index">
+        <!-- <mu-sub-header>someone
+        </mu-sub-header> -->
         <mu-list-item title="Mike Li">
-        <mu-avatar src="/images/avatar1.jpg" slot="leftAvatar"/>
-        <mu-icon value="chat_bubble" slot="right"/>
+        <mu-avatar src="http://ossweb-img.qq.com/images/lol/img/champion/Caitlyn.png" slot="leftAvatar"/>
+        <mu-icon value="location_on" slot="right"/>
         </mu-list-item>
     </mu-list>
   </div>
@@ -21,6 +22,15 @@
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      links: [1, 2, 3, 4, 5, 6]
+    };
+  },
+  methods: {
+    goback() {
+      this.$router.go(-1);
+    }
+  }
+};
 </script>
