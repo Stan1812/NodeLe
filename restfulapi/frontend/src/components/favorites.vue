d37e5813abdd476de9a3b6fc36bd0d108af787c1
stan1812 <2314980309@qq.com>
2017-11-28 22:50:58 +0800

    dont-konw-what page

--- restfulapi/frontend/src/components/favorites.vue
+++ restfulapi/frontend/src/components/favorites.vue
@@ -1,20 +1,44 @@
 <template>
-<div> this is a favorite page</div>  
+<div id="wrapper">
+    <div id="title">我也不知道这个页面要放些什么</div>
+    <!-- <div v-html="compiledMarkdown" id="reviewer"></div> -->
+</div>
 </template>
 
 <script>
 export default {
-data(){
+  data() {
     return {
-
+      input: "### hello"
+    };
+  },
+  computed: {
+    compiledMarkdown: function() {
+      return marked(this.input, { sanitize: true });
     }
-},
-methods:{
-
-}
-}
+  },
+  methods: {}
+};
 </script>
 
 <style>
-
-</style>
+#wrapper {
+  background-attachment: fixed;
+  background: url("http://ossweb-img.qq.com/images/lol/web201310/skin/big22009.jpg")
+    no-repeat center center;
+  min-width: 100vw;
+  min-height: 85vh;
+  background-size: cover;
+  display: flex;
+  flex-direction: column-reverse;
+}
+#title {
+  position: relative;
+  bottom: 0;
+  width: 100vw;
+  line-height: 20vh;
+  color: aliceblue;
+  font-size: 1rem;
+  text-align: center;
+}
+</style>                                           
